25a0b47806390be3ad731e07f9afe4c17674f4d4
Aditya7880900936 <adityasanskarsrivastav788@gmail.com>
2025-02-28 06:03:13 +0530

    fix errors

--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -1,14 +1,36 @@
 const express = require('express');
 const { register, verifyOTP, resendOTP, login, logout, dashboard } = require('../Controllers/authController');
-const authMiddleware = require('../MiddleWare/authmiddleware');
-
+const User = require('../Models/User');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
+const protectRoute = async(req,res,next)=>{
+    try {
+        const token = req.cookies.jwt
+        if(!token){
+            return res.status(401).json({message:"Unauthorized-No token Provided"})
+        }
+        const decoded = jwt.verify(token,process.env.JWT_SECRET);
+        if(!decoded){
+            return res.status(401).json({message:"Unauthorized-No token Provided"});
+        }
+        const user = await User.findById(decoded.userId).select("-password");
+        if(!user){
+            return res.status(404).json({message:"User Not Found"});
+        }
+        req.user = user;
+        next();
 
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
 router.post('/register', register);
 router.post('/verify-otp', verifyOTP);
 router.post('/resend-otp', resendOTP);
 router.post('/login', login);
 router.post('/logout', logout);
-router.get('/dashboard', authMiddleware, dashboard);
+router.get('/dashboard', protectRoute, dashboard);
 
 module.exports = router;
+
