304e99b45d2cd50d74c642343ea8fb8ad63dd99e
Aditya7880900936 <adityasanskarsrivastav788@gmail.com>
2025-03-01 22:32:55 +0530

    fix the origin in cors

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-   origin:"http://localhost:5173",
+   origin:"https://e-commerce-revispy.vercel.app/",
    credentials:true,
    methods: ['GET', 'POST'],
    allowedHeaders: ['Content-Type', 'Authorization']
