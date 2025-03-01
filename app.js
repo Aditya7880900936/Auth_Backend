2c609062712dd1f9ef48847873420a6e9ddbc0fd
Aditya7880900936 <adityasanskarsrivastav788@gmail.com>
2025-03-01 22:38:31 +0530

    please ab to shi ho jaa

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-   origin:"https://e-commerce-revispy.vercel.app/",
+   origin:"https://e-commerce-revispy.vercel.app",
    credentials:true,
    methods: ['GET', 'POST'],
    allowedHeaders: ['Content-Type', 'Authorization']
